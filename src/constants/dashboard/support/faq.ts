3f81a4a32363c3a0c015392d2327f5571ba2e39f
Anurag Samarth <asamarth017@gmail.com>
2024-12-13 19:28:53 +0530

    ADDED: whitelist and support section.

---
+++ src/constants/dashboard/support/faq.ts
@@ -0,0 +1,28 @@
+type TFaq = {
+  id: string;
+  question: string;
+  answer: string;
+};
+
+export const Faq: TFaq[] = [
+  {
+    id: '1',
+    question: 'What happens in case of a dispute?',
+    answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, ad.',
+  },
+  {
+    id: '2',
+    question: 'How do I contact Payafta support?',
+    answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, corporis.',
+  },
+  {
+    id: '3',
+    question: 'What happens if I request for delivery time extension and buyer ignores it?',
+    answer: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, quibusdam!',
+  },
+  {
+    id: '4',
+    question: 'What are the other benefits of using Payafta for my financial transactions?',
+    answer: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, impedit.',
+  },
+];
