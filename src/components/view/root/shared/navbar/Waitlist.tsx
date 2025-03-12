d6d253afc026c097bd259c8d2004d7367c10288a
Anurag Samarth <asamarth017@gmail.com>
2025-03-12 23:22:53 +0530

    MODIFIED: modified the waitlist and footer.

--- src/components/view/root/shared/navbar/Waitlist.tsx
+++ src/components/view/root/shared/navbar/Waitlist.tsx
@@ -19,10 +19,12 @@ import {
 import { Form } from '@/components/ui/form';
 import { addToWhitelist } from '@/lib/actions/auth/utils.actions';
 import { TWhiteList, whiteListSchema } from '@/lib/validations/Utils';
+import { RePhoneNumberInput } from '@/components/re-ui/re-input/RePhoneNumberInput';
 
 const defaultValues = {
   name: '',
   email: '',
+  phoneNo: '',
 };
 
 export function Waitlist() {
@@ -61,12 +63,16 @@ export function Waitlist() {
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div className="mt-3 space-y-4">
               <div>
-                <ReHeading heading="Full Name" />
+                <ReHeading heading="Full Name" size="base" className="text-gray-700" />
                 <ReInput name="name" />
               </div>
               <div>
-                <ReHeading heading="Email" size="lg" />
+                <ReHeading heading="Email" size="base" className="text-gray-700" />
                 <ReInput name="email" />
+              </div>
+              <div>
+                {/* <ReHeading heading="Phone Number" /> */}
+                <RePhoneNumberInput name="phoneNo" />
               </div>
             </div>
 
