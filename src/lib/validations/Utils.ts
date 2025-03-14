ee47e73b32085453da4716cc831097bf114a2b38
Anurag Samarth <asamarth017@gmail.com>
2025-03-14 19:57:44 +0530

    MODIFIED: modified the waitlist with new api.

--- src/lib/validations/Utils.ts
+++ src/lib/validations/Utils.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 export const whiteListSchema = z.object({
   name: z.string().min(1, 'Full name is required'),
   email: z.string().email('Invalid email address'),
-  phoneNo: z.string().min(1, 'Phone number is required'),
+  phoneNumber: z.string().min(1, 'Phone number is required'),
 });
 export type TWhiteList = z.infer<typeof whiteListSchema>;
