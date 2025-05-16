a163b28e61c3ed9c984afac726e466370549445a
Tahdi Islam <tshdi60@gmail.com>
2025-05-16 15:36:55 +0600

    ADDED: integrated api for orders.

--- src/components/view/adminDashboard/PaymentOrder/index.tsx
+++ src/components/view/adminDashboard/PaymentOrder/index.tsx
@@ -2,31 +2,31 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 import { ReDataTable } from '../shared/ReDateTable';
 
-import FilterDataSection from './FilterDataSection';
-import { DataTable } from './DataTable';
+// import { DataTable } from './DataTable';
+import { TOrder, TOrderResponse } from '@/types/admin/orders.type';
 
-export type Payment = {
-  ordersId: string;
-  name: string;
-  type: string;
-  amount: string;
-  status: string;
-};
-
-const columns: ColumnDef<Payment>[] = [
+const columns: ColumnDef<TOrder>[] = [
   {
-    accessorKey: 'ordersId',
+    accessorKey: 'id',
     header: 'ORDERS ID',
   },
   {
-    accessorKey: 'name',
-    header: 'CREATED BY',
+    accessorKey: 'sellerFirstName',
+    header: 'Seller',
+    cell({ row }) {
+      return (
+        <div>
+          {row.original.sellerFirstName} {row.original.sellerLastName}
+        </div>
+      );
+    },
   },
   {
-    accessorKey: 'type',
+    accessorKey: 'transactionType',
     header: 'TYPE',
   },
   {
@@ -41,9 +41,12 @@ const columns: ColumnDef<Payment>[] = [
 
       const styles =
         {
-          Active: 'bg-[#E9F5FB] text-[#0F973C] text-center py-1 text-sm font-medium font-inter',
-          Pending: 'bg-[#E9F5FB] text-[#1F7EAD] text-center py-1 text-sm font-medium font-inter',
-          Suspended: 'bg-[#FCE9E9] text-[#D42620] text-center py-1 text-sm font-medium font-inter',
+          AGREEMENT: 'bg-[#E0F7FA] text-[#00796B] text-center py-1 text-sm font-medium font-inter',
+          PAYMENT: 'bg-[#FFF3E0] text-[#F57C00] text-center py-1 text-sm font-medium font-inter',
+          SHIPPING: 'bg-[#E3F2FD] text-[#1976D2] text-center py-1 text-sm font-medium font-inter',
+          DELIVERY: 'bg-[#F1F8E9] text-[#33691E] text-center py-1 text-sm font-medium font-inter',
+          CLOSED: 'bg-[#ECEFF1] text-[#455A64] text-center py-1 text-sm font-medium font-inter',
+          DISPUTED: 'bg-[#FFEBEE] text-[#C62828] text-center py-1 text-sm font-medium font-inter',
         }[status] || '';
 
       return <div className={styles}>{status}</div>;
@@ -52,106 +55,86 @@ const columns: ColumnDef<Payment>[] = [
   },
 ];
 
-const tData = [
-  {
-    ordersId: 'US-123456789',
-    name: 'John Doe',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Pending',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Abram Lipshutz',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Tiana Bergson',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Cristofer Dias',
-    type: 'Services',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Kadin Workman',
-    type: 'Services',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Wilson Aminoff',
-    type: 'Services',
-    amount: '₦200,000.00',
-    status: 'Suspended',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Phillip Passaquindici Arcand',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Kianna Bator',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Suspended',
-  },
-  {
-    ordersId: 'US-123456789',
-    name: 'Tiana Levin',
-    type: 'Product',
-    amount: '₦200,000.00',
-    status: 'Active',
-  },
-];
-
 interface PageChangeParams {
   pageNumber?: number;
   selectedDate?: string;
-  Status?: string;
+  Status?: {
+    Status: string;
+  };
 }
 
 export default function PaymentOrder() {
-  const [data, setData] = useState<Payment[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [page, setPage] = useState(1);
-  const [totalCount, setTotalCount] = useState(0);
   const pageSize = 8;
+  const [orders, setOrders] = useState<TOrderResponse>({} as TOrderResponse);
 
-  function handlePageChange(params: PageChangeParams = {}) {
-    const { pageNumber = 1, selectedDate = 'Today', Status = 'Active' } = params;
-    try {
-      console.log({ pageNumber, selectedDate, Status });
-      setTimeout(() => {
-        setTotalCount(tData.length);
-        setData(tData);
-        setPage(pageNumber);
-        setIsLoading(false);
-      }, 500);
-    } catch (error) {
-      console.error('Error loading data:', error);
-      setIsLoading(false);
-      setData([]);
+  const handleOrderFilterChange = async (
+    params: PageChangeParams = {
+      pageNumber: 1,
+      selectedDate: '',
+      Status: { Status: '' },
     }
-  }
+  ) => {
+    console.log('🌼 🔥🔥 handleTransactionFilterChange 🔥🔥 params🌼', params);
+
+    // Optional: Handle filter change
+    setIsLoading(true);
+
+    let startDate, endDate;
+
+    // Check if the value is a date range like "May 5, 2025 - May 7, 2025"
+    if (params?.selectedDate && params?.selectedDate?.includes(' - ')) {
+      const date = params?.selectedDate?.split(' - ');
+
+      const startDateI = new Date(date[0]);
+      const endDateI = new Date(date[1]);
+
+      const formattedStart = startDateI.toISOString().split('T')[0]; // "YYYY-MM-DD"
+      const formattedEnd = endDateI.toISOString().split('T')[0]; // "YYYY-MM-DD"
+
+      startDate = formattedStart;
+      endDate = formattedEnd;
+      params.selectedDate = '';
+    }
+
+    console.log('🌼 🔥🔥 Users 🔥🔥 startDate🌼', startDate);
+    console.log('🌼 🔥🔥 Users 🔥🔥 endDate🌼', endDate);
+
+    setPage(params?.pageNumber || 1);
+
+    console.log('🌼 🔥🔥 handleFilterChange 🔥🔥 page🌼', page);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/orders?status=${params?.Status?.Status === 'all' ? '' : params?.Status?.Status}&createdAt=${params?.selectedDate === 'all' ? '' : params?.selectedDate}&sortBy=createdAt&sortOrder=desc&page=${params?.pageNumber}&limit=${pageSize}&startDate=${startDate}&endDate=${endDate}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // authorization: session?.accessToken as string,
+          },
+          cache: 'no-store',
+        }
+      );
+      const data = await response.json();
+
+      console.log('🌼 🔥🔥 handleTransactionFilterChange 🔥🔥 data🌼', data);
+
+      if (data?.success) {
+        setOrders(data?.data);
+      } else {
+        toast.error(data?.errorName || 'Failed to load users');
+      }
+    } catch (error) {
+      toast.error(error instanceof Error ? error.message : 'Failed to load users');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    handlePageChange({ pageNumber: 1 });
-
-    setData(tData);
+    handleOrderFilterChange({ pageNumber: 1 });
   }, []);
 
   return (
@@ -159,11 +142,11 @@ export default function PaymentOrder() {
       <div className="rounded-md bg-white p-5">
         <ReDataTable
           columns={columns}
-          data={data}
+          data={orders?.data}
           isLoading={isLoading}
-          onPageChange={handlePageChange}
+          onPageChange={handleOrderFilterChange}
           rowClickMode="none"
-          count={totalCount}
+          count={orders?.meta?.total}
           page={page}
           setPage={setPage}
           pageSize={pageSize}
@@ -177,9 +160,12 @@ export default function PaymentOrder() {
               placeholder: 'Select a State',
               options: [
                 { label: 'All', value: 'all' },
-                { label: 'Successful', value: 'Successful' },
-                { label: 'Failed', value: 'Failed' },
-                { label: 'Suspended', value: 'Suspended' },
+                { label: 'AGREEMENT', value: 'AGREEMENT' },
+                { label: 'PAYMENT', value: 'PAYMENT' },
+                { label: 'SHIPPING', value: 'SHIPPING' },
+                { label: 'DELIVERY', value: 'DELIVERY' },
+                { label: 'CLOSED', value: 'CLOSED' },
+                { label: 'DISPUTED', value: 'DISPUTED' },
               ],
             },
           ]}
